9ff0b507f60079062417db924526caede24cbb8a
realadityakumar <adityakumar23214@lpu.in>
2024-04-14 23:47:46 +0530

    h

---
+++ index.js
@@ -0,0 +1,13 @@
+
+console.log("Hello d");
+gsap.to("scrambled", {
+    scrambleText: {
+      text: "Understand Food Resource Dynamics in India!",
+      chars: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+      speed: 0.3
+    },
+    repeat: -1,
+    yoyo: true,
+    repeatDelay: 1
+  });
+  console.log("Hello World");
